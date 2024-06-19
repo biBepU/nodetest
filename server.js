275de64386248a410d39004369e8f96fd4dec4fc
htet <zerofill.sg@gmail.com>
2024-06-19 22:51:50 +0630

    routetest

--- server.js
+++ server.js
@@ -1,10 +1,55 @@
 const http = require('http');
+const fs = require('fs');
+
+
+
+
 
 const server = http.createServer((req,res)=>{
-    res.setHeader('Content-type','text/html');
-    res.write('<h1>Hello World<h1>');
+    let fileName;
+    switch(req.url){
+        case '/':
+            fileName = "home.html";
+            res.statusCode = 200;
+        break;
 
-    res.end();
+        case '/contact':
+            fileName = "contact.html";
+            res.statusCode = 200;
+        break;
+        case '/contact-us':
+           
+            res.statusCode = 301;
+            res.setHeader ('location','/contact')
+        break;
+
+        case '/about':
+            fileName = "about.html";
+            res.statusCode = 200;
+        break;
+
+        default:
+            fileName = "404.html";
+            res.statusCode = 404;
+        break;
+    }
+
+
+    res.setHeader('Content-type','text/html');
+    fs.readFile('./views/'+fileName,(err,data)=>{
+        if(err){
+            console.log(err)
+            res.end();
+
+        }else{
+            res.write(data);
+            res.end();
+        }
+
+    })
+  
+
+  
 
 })
 
