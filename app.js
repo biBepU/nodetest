bb37ef57c64d73194b8d6f9c285b5f404463eeff
htet <zerofill.sg@gmail.com>
2024-06-21 23:05:14 +0630

    mongodb day1

--- app.js
+++ app.js
@@ -1,21 +1,53 @@
 const express = require('express')
 
 let morgan = require('morgan')
+const mongoose = require('mongoose')
+
+const Blog = require('./models/Blogs')
 
 const app = express();
+
+let mongoUrl ='mongodb+srv://zerofillsg:Htet+Eiwai@htetmyat.6ifhafs.mongodb.net/?retryWrites=true&w=majority&appName=htetmyat'
+
+mongoose.connect(mongoUrl).then(()=>{
+  console.log('connect to db')
+  app.listen(3000,()=>{
+    console.log('app is running on local host 3000')
+  })
+}).catch(e=>
+  console.log(e)
+)
+
 app.set('views', './views')
 app.set('view engine', 'ejs')
 
 app.use(morgan('dev'));
 
+
 app.use(express.static('public'))
 
-app.get('/',(req,res)=>{
-  let blogs = [
-    {title:'blog title 1',intro:'this is the blog intro 1'},
-    {title:'blog title 2',intro:'this is the blog intro 2'},
-    {title:'blog title 2',intro:'this is the blog intro 2'}
-  ]
+app.get('/add-blogs',async(req,res)=>{
+  let blog = new Blog({
+    title : "blog title 3",
+    intro : "blog intro 3",
+    body : "blog body 3"
+  });
+  await blog.save();
+  res.send("blog saved")
+
+})
+
+
+
+app.get('/',async(req,res)=>{
+  // let blogs = [
+  //   {title:'blog title 1',intro:'this is the blog intro 1'},
+  //   {title:'blog title 2',intro:'this is the blog intro 2'},
+  //   {title:'blog title 2',intro:'this is the blog intro 2'}
+  // ]
+
+  let blogs =await Blog.find().sort({createdAt : -1})
+  console.log(blogs)
   res.render('home',
     {
       blogs,
@@ -23,6 +55,11 @@ app.get('/',(req,res)=>{
     }
   )
 });
+
+app.get('/single-blog',async(req,res)=>{
+  let blog = await Blog.findById('6675a954c063b7b713fef9b1')
+  res.json(blog)
+})
 app.get('/about',(req,res)=>{
     res.render('about',
         {title : "About"}
@@ -49,4 +86,3 @@ app.get('/about',(req,res)=>{
   })
 
 
-app.listen(3000)
